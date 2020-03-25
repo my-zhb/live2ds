8ae7e73855174b976519a0b26c3a83d5e515b6ba
myiszhb@gmail.cim <myiszhb@gmail.com>
2020-03-25 18:13:07 +0800

    july

--- waifu-tips.js
+++ waifu-tips.js
@@ -176,7 +176,7 @@ function loadWidget(config) {
 		if (modelId === null) {
 			// 首次访问加载 指定模型 的 指定材质
 			var modelId = 6, // 模型 ID
-				modelTexturesId = 19; // 材质 ID
+				modelTexturesId = 9; // 材质 ID
 		}
 		loadModel(modelId, modelTexturesId);
 		fetch(waifuPath)
